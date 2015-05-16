a9c5d04c62aa65378062137e0cdd2c436499fad7
Chris Stewart <cstewart@bignerdranch.com>
2015-05-16 11:08:37 -0400

    Initial commit

---
+++ Routes/app/src/main/java/com/cstewart/android/routes/RouteActivity.java
@@ -0,0 +1,15 @@
+package com.cstewart.android.routes;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class RouteActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_route);
+    }
+
+}
