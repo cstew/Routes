4558660b2e16d5e783a29c3c31c61fa440a2a77d
Chris Stewart <cstewart@bignerdranch.com>
2015-06-07 20:57:44 -0400

    A little cleanup and starting to implement the ftue viewpager

--- Routes/app/src/main/java/com/cstewart/android/routes/controller/FtueActivity.java
+++ Routes/app/src/main/java/com/cstewart/android/routes/controller/FtueActivity.java
@@ -3,11 +3,16 @@ package com.cstewart.android.routes.controller;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.cstewart.android.routes.R;
 
 public class FtueActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
 
     public static Intent newIntent(Context context) {
         return new Intent(context, FtueActivity.class);
@@ -17,6 +22,22 @@ public class FtueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ftue);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_ftue_viewpager);
+        mViewPager.setAdapter(new FtueAdapter());
+    }
+
+    private class FtueAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
     }
 
 }
